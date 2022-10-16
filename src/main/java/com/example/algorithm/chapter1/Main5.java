4b32162edd509b776858d8ac3f5d6d844a29f595
sjlee <sooji7876@gmail.com>
2022-10-16 15:01:59 +0900

    2022.10.16 5장 실패, 6장 완료 (수정)

--- src/main/java/com/example/algorithm/chapter1/Main5.java
+++ src/main/java/com/example/algorithm/chapter1/Main5.java
@@ -39,3 +39,31 @@ public class Main5 {
 //[출력]
 //첫 줄에 알파벳만 뒤집힌 문자열을 출력합니다.
 //S#T!EG*b@a
+
+//class Main {
+//    public String solution(String str){
+//        String answer;
+//        char[] s=str.toCharArray();
+//        int lt=0, rt=str.length()-1;
+//        while(lt<rt){
+//            if(!Character.isAlphabetic(s[lt])) lt++;
+//            else if(!Character.isAlphabetic(s[rt])) rt--;
+//            else{
+//                char tmp=s[lt];
+//                s[lt]=s[rt];
+//                s[rt]=tmp;
+//                lt++;
+//                rt--;
+//            }
+//        }
+//        answer=String.valueOf(s);
+//        return answer;
+//    }
+//
+//    public static void main(String[] args){
+//        Main T = new Main();
+//        Scanner kb = new Scanner(System.in);
+//        String str=kb.next();
+//        System.out.println(T.solution(str));
+//    }
+//}
--- src/main/java/com/example/algorithm/chapter1/Main6.java
+++ src/main/java/com/example/algorithm/chapter1/Main6.java
@@ -34,32 +34,3 @@ public class Main6 {
 //[출력]
 //첫 줄에 중복문자가 제거된 문자열을 출력합니다.
 //kset
-
-
-//class Main {
-//    public String solution(String str){
-//        String answer;
-//        char[] s=str.toCharArray();
-//        int lt=0, rt=str.length()-1;
-//        while(lt<rt){
-//            if(!Character.isAlphabetic(s[lt])) lt++;
-//            else if(!Character.isAlphabetic(s[rt])) rt--;
-//            else{
-//                char tmp=s[lt];
-//                s[lt]=s[rt];
-//                s[rt]=tmp;
-//                lt++;
-//                rt--;
-//            }
-//        }
-//        answer=String.valueOf(s);
-//        return answer;
-//    }
-//
-//    public static void main(String[] args){
-//        Main T = new Main();
-//        Scanner kb = new Scanner(System.in);
-//        String str=kb.next();
-//        System.out.println(T.solution(str));
-//    }
-//}
